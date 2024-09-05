994cfd38e00882642ec49a0dc48522d674d990bd
Remi Sha <cestnana@gmail.com>
2024-09-06 03:39:05 +0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Remi Sha on 2024/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
